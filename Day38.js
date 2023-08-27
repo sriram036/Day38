f610b349ec9600f6fa2e50c61024462faeb175a6
sriram036 <sriramravichandran2608@gmail.com>
2023-08-27 10:06:40 +0530

    Added : Day38 Problems

---
+++ Day38.js
@@ -0,0 +1,17 @@
+console.log('Hello World from Node js');
+
+age = 20;
+if (age >= 18) {
+    console.log("Student is Eligible.")
+} else {
+    console.log("Student is not Eligible.")
+}
+
+var isPresent = 0;
+let employee = Math.floor(Math.random() * 10) % 2;
+if (employee == isPresent) {
+    console.log("Employee is Absent.");
+}
+else {
+    console.log("Employee is Present.");
+}
